d3622d6893ba6bcf2e700e196a7440e2bdff6a86
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-07-05 14:30:39 +0200

    set up project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const app = require('./server.js')
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`\n Server is running on http://localhost:${port}\n`);
+});
